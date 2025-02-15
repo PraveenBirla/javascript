10ffb0af1f8c600b10dc739f4d4fd73f215b7cda
pareveen-birla <birlap409@gmail.com>
2025-02-15 22:20:05 +0530

    third day

---
+++ Object13.js
@@ -0,0 +1,43 @@
+// const person = {
+//   firstname :"praveen" ,
+//   lastname:"Birla",
+//   age: 18 ,
+//   addrss :"vidisha"
+// } 
+
+// console.log(person)
+// console.log(person.firstname) 
+
+// const person2 = {
+//    fullname :  function(){
+//     return this.firstname + "age" + this.age
+// } 
+// }
+
+// person2.firstname = "kazim" 
+// person2.age=90 
+// console.log(person2)
+
+// const person3 = new Object()
+
+// person3.firstname ="prajjwal"
+// person3.age = 20
+// console.log(person3)
+
+// console.log(person2.firstname)
+// console.log(person2["age"]) 
+
+
+
+// console.log(person2.fullname())
+
+const person4 = {
+  firstnname :"somdeep",
+  age: 34 ,
+  Address:"Tkm"
+} 
+
+const x = person4 
+x.age = 67 
+
+console.log(person4)
---
+++ ObjectSTwo14.js
@@ -0,0 +1,10 @@
+const person = {
+  firstName : "praveen",
+  age : 23 ,
+   mycars :{
+    car1 :"Ford",
+    car2:"BMW"
+   }
+} 
+
+console.log(person.mycars.car1)  
